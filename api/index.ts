c0fc82dfd6e5adf9a35a0e2bb4628090f49ead9c
conversioao <conversioao@gmail.com>
2026-03-02 17:14:54 +0000

    k

--- api/index.ts
+++
@@ -1,6 +0,0 @@
-
-import { createServer } from 'http';
-import app from '../src/api.js';
-
-// No Vercel, o ambiente será production, garantindo o "export default app;" no api.js
-export default app;
